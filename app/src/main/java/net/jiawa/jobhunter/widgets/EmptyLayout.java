475ce9f0d31a95eed945152a4cf123a56ef7f0a7
xixia <xixia@jiawa.net>
2017-03-26 22:13:18 +0800

    [xixia][20170326][ 1.用代码创建EmptyLayout ]

--- app/src/main/java/net/jiawa/jobhunter/widgets/EmptyLayout.java
+++ app/src/main/java/net/jiawa/jobhunter/widgets/EmptyLayout.java
@@ -2,14 +2,14 @@ package net.jiawa.jobhunter.widgets;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
+import android.view.Gravity;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import net.jiawa.jobhunter.R;
-import net.jiawa.jobhunter.utils.ViewUtils;
 
 /**
  * Created by lenovo on 2017/3/25.
@@ -25,7 +25,7 @@ import net.jiawa.jobhunter.utils.ViewUtils;
  * 当加载完成后,隐藏这个界面
  * 显示用户数据
  */
-public class EmptyLayout extends FrameLayout {
+public class EmptyLayout extends LinearLayout {
 
     public static final int HIDE_LAYOUT = 4;
     public static final int NETWORK_ERROR = 1;
@@ -51,10 +51,36 @@ public class EmptyLayout extends FrameLayout {
 
     TextView mStatus;
     ProgressBar mLoading;
+
+    /**
+     * 用代码动态创建这个EmptyLayout,
+     * 减少依赖文件
+     */
     private void init() {
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.view_empty_layout, this);
-        mStatus = ViewUtils.findViewById(view, R.id.tv_emptylayout_status);
-        mLoading = ViewUtils.findViewById(view, R.id.pb_emptylayout_loading);
+
+        // 设置当前EmptyLayout的orientation为垂直方向
+        this.setOrientation(LinearLayout.VERTICAL);
+        // 设置两个子View居中显示
+        this.setGravity(Gravity.CENTER);
+        // 设置空白页的背景色是白色
+        this.setBackgroundColor(getContext().getResources().getColor(R.color.white_ffffff));
+        // 让里面的两个控件往上靠一点
+        this.setPadding(this.getPaddingLeft(), this.getPaddingTop(), this.getPaddingRight(), 350);
+
+        mLoading = new ProgressBar(getContext());
+        LinearLayout.LayoutParams lpLoading = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        mLoading.setLayoutParams(lpLoading);
+
+        mStatus = new TextView(getContext());
+        LinearLayout.LayoutParams lpStatus = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        // 让下面的字离上面的Loading框有一段距离
+        lpStatus.topMargin = 30;
+        mStatus.setLayoutParams(lpStatus);
+
+        this.addView(mLoading);
+        this.addView(mStatus);
     }
 
     public void setType(int id) {
