c7a4d7ce499180acd18ddba7ceb817adbda119d7
xixia <xixia@jiawa.net>
2017-04-01 15:22:36 +0800

    [xixia][20170401][ 1.Add content bean ]

---
+++ app/src/main/java/net/jiawa/jobhunter/bean/git/projectdetail/Links.java
@@ -0,0 +1,37 @@
+package net.jiawa.jobhunter.bean.git.projectdetail;
+
+import java.io.Serializable;
+
+/**
+ * Auto-generated: 2017-04-01 14:46:41
+ *
+ * @author www.jsons.cn 
+ * @website http://www.jsons.cn/json2java/ 
+ */
+public class Links implements Serializable {
+
+    private String self;
+    private String git;
+    private String html;
+    public void setSelf(String self) {
+         this.self = self;
+     }
+     public String getSelf() {
+         return self;
+     }
+
+    public void setGit(String git) {
+         this.git = git;
+     }
+     public String getGit() {
+         return git;
+     }
+
+    public void setHtml(String html) {
+         this.html = html;
+     }
+     public String getHtml() {
+         return html;
+     }
+
+}
