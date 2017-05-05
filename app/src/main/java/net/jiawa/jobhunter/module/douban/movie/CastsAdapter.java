9271a95aa8c5416f7bea3a134c8abadadc6aa419
xixia <xixia@jiawa.net>
2017-05-05 15:17:55 +0800

    [xixia][20170505][ 1.布局调整 ]

--- app/src/main/java/net/jiawa/jobhunter/module/douban/movie/CastsAdapter.java
+++ app/src/main/java/net/jiawa/jobhunter/module/douban/movie/CastsAdapter.java
@@ -31,15 +31,15 @@ public class CastsAdapter extends BaseGeneralRecyclerAdapter<Object> {
     @Override
     protected void onBindDefaultViewHolder(RecyclerView.ViewHolder holder, Object item, int position) {
         String image = "";
-        String title = "";
+        // String title = "";
         String name = "";
         if (item instanceof Casts) {
             image  = ((Casts) item).getAvatars().getLarge();
-            title = "饰演";
+            // title = "饰演";
             name = ((Casts) item).getName();
         } else if (item instanceof Directors) {
             image  = ((Directors) item).getAvatars().getLarge();
-            title = "导演";
+            // title = "导演";
             name = ((Directors) item).getName();
         } else {
             throw new RuntimeException("item in CastsAdapter must be Casts or Directors!");
@@ -50,20 +50,20 @@ public class CastsAdapter extends BaseGeneralRecyclerAdapter<Object> {
                 .asBitmap()
                 .placeholder(R.mipmap.ic_launcher)
                 .into(h.avater);
-        h.title.setText(title);
+        // h.title.setText(title);
         h.name.setText(name);
     }
 
     private static class CastsViewHolder extends RecyclerView.ViewHolder {
 
         ImageView avater;
-        TextView title;
+        // TextView title;
         TextView name;
 
         public CastsViewHolder(View itemView) {
             super(itemView);
             avater = (ImageView) itemView.findViewById(R.id.iv_avater);
-            title = (TextView) itemView.findViewById(R.id.tv_title);
+            // title = (TextView) itemView.findViewById(R.id.tv_title);
             name = (TextView) itemView.findViewById(R.id.tv_name);
         }
     }
